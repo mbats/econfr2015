387149444104825fdf65f9d541b90aa9d073d6cf
Mélanie Bats <melanie.bats@obeo.fr>
2015-06-14 14:54:48 +0200

    Initial state

---
+++ org.eclipse.sirius.sample.basicfamily.design/src/org/eclipse/sirius/sample/basicfamily/services/FamilyServices.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2014 Obeo
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Obeo - initial API and implementation
+ */
+package org.eclipse.sirius.sample.basicfamily.services;
+
+import org.eclipse.sirius.sample.basicfamily.Person;
+
+public class FamilyServices {
+
+	public int getCousinsNumber(Person person) {
+		// TODO Complete the getCousinsNumber service
+		return 0;
+	}
+}
